5119c51b36086923866cc487d03f79e26d13f6bb
Daz DeBoer <daz@gradle.com>
2018-08-01 12:39:05 -0600

    Better handling of empty selectors
    - Ignore them, unless _all_ selectors are empty.
    
    Better handling for empty

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/resolveengine/graph/selectors/SelectorStateResolver.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/resolveengine/graph/selectors/SelectorStateResolver.java
@@ -71,12 +71,16 @@ public class SelectorStateResolver<T extends ComponentResolutionState> {
     private List<T> resolveSelectors(List<? extends ResolvableSelectorState> selectors, VersionSelector allRejects) {
         if (selectors.size() == 1) {
             ResolvableSelectorState selectorState = selectors.get(0);
-            ComponentIdResolveResult resolved = selectorState.resolve(allRejects);
-            T selected = SelectorStateResolverResults.componentForIdResolveResult(componentFactory, resolved, selectorState);
-            return Collections.singletonList(selected);
+            return resolveSingleSelector(selectorState, allRejects);
         }
 
         return buildResolveResults(selectors, allRejects);
+    }
+
+    private List<T> resolveSingleSelector(ResolvableSelectorState selectorState, VersionSelector allRejects) {
+        ComponentIdResolveResult resolved = selectorState.resolve(allRejects);
+        T selected = SelectorStateResolverResults.componentForIdResolveResult(componentFactory, resolved, selectorState);
+        return Collections.singletonList(selected);
     }
 
     /**
@@ -86,12 +90,14 @@ public class SelectorStateResolver<T extends ComponentResolutionState> {
      */
     private List<T> buildResolveResults(List<? extends ResolvableSelectorState> selectors, VersionSelector allRejects) {
         SelectorStateResolverResults results = new SelectorStateResolverResults(selectors.size());
+        boolean hasResolution = false;
         for (ResolvableSelectorState selector : selectors) {
-
-            // For a 'reject-only' selector, don't need to resolve
-//            if (isRejectOnly(dep)) {
-//                continue;
-//            }
+            // If this selector doesn't specify a prefer/require version, then ignore it here.
+            // This will avoid resolving 'reject' selectors, too.
+            if (isEmpty(selector)) {
+                selector.markResolved();
+                continue;
+            }
 
             // Check already resolved results for a compatible version, and use it for this dependency rather than re-resolving.
             if (results.alreadyHaveResolution(selector)) {
@@ -103,8 +109,23 @@ public class SelectorStateResolver<T extends ComponentResolutionState> {
             ComponentIdResolveResult resolved = selector.resolve(allRejects);
 
             results.registerResolution(selector, resolved);
+            hasResolution = true;
         }
+
+        if (!hasResolution) {
+            // Every selector was empty: 'resolve' one of them
+            return resolveSingleSelector(selectors.get(0), allRejects);
+        }
+
         return results.getResolved(componentFactory);
+    }
+
+    private boolean isEmpty(ResolvableSelectorState selector) {
+        ResolvedVersionConstraint versionConstraint = selector.getVersionConstraint();
+        if (versionConstraint != null) {
+            return versionConstraint.getPreferredSelector().getSelector().isEmpty();
+        }
+        return false;
     }
 
     private VersionSelector createAllRejects(List<? extends ResolvableSelectorState> selectors) {
